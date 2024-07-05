2f9280627879b6c11b8243b6abadda7649d1852e
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-07-05 10:15:30 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Bob",
+    address: "27 Pearl St",
+}
+function updateEmployeeWithKeyAndValue(employee, state, stateName)
+{
+    const newEmployee = {...employee}; 
+    newEmployee [state] = stateName;
+    return newEmployee;
+}
+//const newEmployee = updateEmployeeWithKeyAndValue(employee, 'state', 'Colorado')
+//console.log(newEmployee)
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, state, stateName) {
+    employee [state] = stateName;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, state, stateName) {
+    const newEmployee = {...employee};
+    newEmployee [state] = stateName;
+    delete newEmployee.state;
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, state, stateName) {
+    employee [state] = stateName;
+    delete employee.state;
+    return employee;
+}
